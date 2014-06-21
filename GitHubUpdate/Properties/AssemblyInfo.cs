c9839e6d450ec3460c90f90388961c5bee473a7d
nixx quality <nixx@is-fantabulo.us>
2014-06-21 10:26:52 +0200

    Added nuspec

--- GitHubUpdate/Properties/AssemblyInfo.cs
+++ GitHubUpdate/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GitHubUpdate")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("GitHub Update Check")]
+[assembly: AssemblyDescription("Easily check if your program is up to date, using GitHub Releases")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("nixx quality")]
 [assembly: AssemblyProduct("GitHubUpdate")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © nixx quality 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
